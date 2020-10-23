3bad17b906083be16c4ff8b6df91f66f98e87f88
Marieta28 <69346003+Marieta28@users.noreply.github.com>
2020-10-24 00:51:07 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 let menuToggle = document.querySelector('#menu-toggle');
 let menu = document.querySelector('.sidebar');
-
- menuToggle.addEventListener('click', 
- function(event)
- {
-   console.log('click');
+menuToggle.addEventListener('click', function(event) {
+   event.preventDefault();
    menu.classList.toggle('visible');
- });
+ })
 
 
 
